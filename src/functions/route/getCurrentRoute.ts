98e5203d9bab9f0bcc43b1318e232d65124b22cb
Diego <martinsdiego0513@outlook.es>
2024-08-29 21:52:06 -0300

    delete negation

--- src/functions/route/getCurrentRoute.ts
+++ src/functions/route/getCurrentRoute.ts
@@ -2,7 +2,7 @@
 export const getCurrentRoute = () => {
 
     const meta_route = import.meta.env;
-    const route = !meta_route.PROD ? meta_route.PUBLIC_GLOBAL_ROUTE : meta_route.PUBLIC_LOCAL_ROUTE;
+    const route = meta_route.PROD ? meta_route.PUBLIC_GLOBAL_ROUTE : meta_route.PUBLIC_LOCAL_ROUTE;
     
     console.log("RUTA ACTUAL: ", route);
 
